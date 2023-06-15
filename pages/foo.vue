a38a32d15f2028e8c92bffde6c5527e826a634bb
kacpereqqo <remzak.k@gmail.com>
2023-06-15 16:41:20 +0200

    test

---
+++ pages/foo.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="foo">
+    <h1 @click="router.push('foo/aaa')">Foo</h1>
+    <NuxtPage />
+  </div>
+</template>
+
+<script setup lang="ts">
+const router = useRouter();
+</script>
+
+<style scoped>
+.foo {
+  color: red;
+  height: 200vh;
+}
+h1 {
+  height: 100%;
+  cursor: pointer;
+}
+</style>
---
+++ pages/foo/[id].vue
@@ -0,0 +1,5 @@
+<template>
+  <h1>bar</h1>
+</template>
+
+<script></script>
